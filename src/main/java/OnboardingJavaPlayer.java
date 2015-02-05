365b7dbe7cb55b1d1bfb58e3a46e0bd2891bf9ae
Jennifer Aguiar <jaguiar@customermatrix.com>
2015-02-05 16:47:54 +0100

    first version Onboarding java like scala

---
+++ src/main/java/OnboardingJavaPlayer.java
@@ -0,0 +1,38 @@
+import math._
+import scala.util._
+
+/**
+ *  * The code below will read all the game information for you.
+ *   * On each game turn, information will be available on the standard input, you will be sent:
+ *    * -> the total number of visible enemies
+ *     * -> for each enemy, its name and distance from you
+ *      * The system will wait for you to write an enemy name on the standard output.
+ *       * Once you have designated a target:
+ *        * -> the cannon will shoot
+ *         * -> the enemies will move
+ *          * -> new info will be available for you to read on the standard input.
+ *           **/
+object OnboardingScalaPlayer {
+
+	def main(args: Array[String]) {
+
+		// game loop
+		//         while(true) {
+		//                     val count = readInt // The number of current enemy ships within range
+		//                                 var minDist = ("",Int.MaxValue)
+		//                                             for(i <- 0 until count) {
+		//                                                             // enemy: The name of this enemy
+		//                                                                             // dist: The distance to your cannon of this enemy
+		//                                                                                             val Array(enemy, _dist) = readLine split " "
+		//                                                                                                             if(_dist.toInt < minDist._2){
+		//                                                                                                                                 minDist = (enemy, _dist.toInt)
+		//                                                                                                                                                 }
+		//                                                                                                                                                             }
+		//                                                                                                                                                                         
+		//                                                                                                                                                                                     // Write an action using println
+		//                                                                                                                                                                                                 // To debug: Console.err.println("Debug messages...")
+		//                                                                                                                                                                                                             
+		//                                                                                                                                                                                                                         println(minDist._1) // The name of the most threatening enemy (HotDroid is just one example)
+		//                                                                                                                                                                                                                                 }
+		//                                                                                                                                                                                                                                     }
+		//                                                                                                                                                                                                                                     }
