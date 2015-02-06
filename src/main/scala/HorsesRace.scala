b48232d265aa7d96773bc7909e10cd208e7a282a
Jennifer Aguiar <jaguiar@customermatrix.com>
2015-02-06 17:01:59 +0100

    horses in scala

---
+++ src/main/scala/HorsesRace.scala
@@ -0,0 +1,38 @@
+import math._
+import scala.util._
+import scala.collection.mutable.TreeSet
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+object Solution {
+
+    def main(args: Array[String]) {
+		val n = readInt
+		val powersSet = new TreeSet[Int]()
+    var continue = true
+		for(i <- 0 until n) {
+		  val pi = readInt
+				if(powersSet(pi)){
+          println(0)
+          continue = false
+					sys.exit()
+				}
+				powersSet += pi
+		}
+
+    var minDist = Int.MaxValue
+    if(continue){
+    powersSet.toSeq.sliding(2).foreach { case Seq(val1,val2) => val dist = (val2-val1).abs 
+      if(dist < minDist) minDist= dist
+      }
+    }
+		 
+
+		// Write an action using println
+		// To debug: Console.err.println("Debug messages...")
+
+		println(minDist)
+	}
+}
